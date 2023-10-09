4eaefc57c6e53d5f812cffc1447118d599394006
fadhel90 <146990552+fadhel90@users.noreply.github.com>
2023-10-09 18:38:18 +0300

    Add files via upload

---
+++ Lab001.py
@@ -0,0 +1,18 @@
+
+
+op2 = min(45,66)
+print(op2)
+print("pramod fadhil")
+66,77
+op2 = max(66,77)
+print(op2)
+out=min(2,3)
+print(out)
+max=(2+65566563)
+print(max)
+
+#print("hello world")
+print("fadhil","nnoor","nathum",sep="+",end="\n")
+print("foofy")
+
+
